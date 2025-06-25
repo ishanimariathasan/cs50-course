c77800c69cf9209807298bb1581cf4ada61a8dbf
ishani <47855437+ishanimariathasan@users.noreply.github.com>
2025-06-25 18:51:02 +0100

    placeholder function to test (#1)

---
+++ search.py
@@ -0,0 +1,7 @@
+def closest():
+    """
+    Find the closest match to a given query in a dataset.
+    This function is a placeholder and should be implemented with actual logic.
+    """
+    pass
+    
